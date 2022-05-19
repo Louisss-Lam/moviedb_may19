bdbb2c6b1c1feb8f515402cbd04e989687648d60
louis <louislam617@gmail.com>
2022-05-19 21:40:54 +0100

    React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Search from './components/Search';
+import axios from 'axios';
 
 function App() {
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  });
+
+  const BASE_URL = 'https://api.themoviedb.org/3/search/movie?api_key=3e21d0634b298df121ea5353d494ab2b&language=en-US&query=';
+
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      axios(BASE_URL + state.s + "&page=1&include_adult=false").then((data) => {
+        console.log(data);
+        let results = data.Search;
+
+        setState(prevState => {
+          return { ...prevState, results: results }
+        })
+      });
+    }
+  }
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return {...prevState, s: s}
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Movie Database</h1>
       </header>
+      <main>
+        <Search handleInput={handleInput} search={search} />
+      </main>
     </div>
   );
 }
